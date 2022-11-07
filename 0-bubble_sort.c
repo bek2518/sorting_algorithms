a0102d44ea7dd15c124a7455fd6850b52bc6d50c
bek2518 <bereketb20@hotmail.com>
2022-11-07 15:31:42 +0300

    Trial

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -19,9 +19,9 @@ void bubble_sort(int *array, size_t size)
 			{
 				if (array[j] > array[j + 1])
 				{
-					tmp = array[j + 1];
-					array[j + 1] = array[j];
-					array[j] = tmp;
+					tmp = array[j];
+					array[j] = array[j + 1];
+					array[j + 1] = tmp;
 					print_array(array, size);
 					counter++;
 				}
