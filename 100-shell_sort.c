225d0e6cec89f38ba973612e4c7a663bddfa7925
bek2518 <bereketb20@hotmail.com>
2022-11-09 15:42:53 +0300

    Betty style edit

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -9,18 +9,19 @@
 void shell_sort(int *array, size_t size)
 {
 	int interval = 1, i, j, temp;
-	
+
 	while (interval < ((int)(size)))
 	{
 		interval = (interval * 3) + 1;
 	}
-	for (interval = (interval - 1) / 3; interval > 0; interval = (interval - 1) / 3)
+	for (interval = (interval - 1) / 3; interval > 0;
+			interval = (interval - 1) / 3)
 	{
 		for (i = interval; i < (int)size; i++)
 		{
 			temp = array[i];
 
-			for (j = i; j >= interval && array[j - interval] > temp; j -= interval) 
+			for (j = i; j >= interval && array[j - interval] > temp; j -= interval)
 			{
 				array[j] = array[j - interval];
 			}
